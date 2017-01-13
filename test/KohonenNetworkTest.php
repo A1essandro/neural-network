9ee58844ade753c70b9803a9308d256aeeb3efdf
A1essandro <alexander.yermakov.1990@gmail.com>
2017-01-13 19:40:29 +0400

    Add test for Kohonen network learning

--- test/KohonenNetworkTest.php
+++ test/KohonenNetworkTest.php
@@ -49,4 +49,29 @@ class KohonenNetworkTest extends PHPUnit_Framework_TestCase
         $this->assertCount(0, $network->getLayers());
     }
 
+    public function testLearn()
+    {
+        $network = new KohonenNetwork([2, 4]);
+        $network->generateSynapses();
+        for ($i = 0; $i < 1500; $i++)
+            $network->learn([mt_rand() / mt_getrandmax(), mt_rand() / mt_getrandmax()]);
+
+        $things = [
+            [0.9, 0.1],
+            [0.05, 0.8],
+            [0, 0.05],
+            [1, 0.89]
+        ];
+
+        for ($a = 0; $a < 4; $a++) {
+            for ($b = 0; $b < 4; $b++) {
+                if ($a == $b)
+                    continue;
+                $this->assertNotEquals($network->input($things[$a])->output(),
+                    $network->input($things[$b])->output());
+            }
+        }
+    }
+
+
 }
